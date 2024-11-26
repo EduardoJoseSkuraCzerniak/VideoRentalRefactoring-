f0c270e9a58b7f2cbeb580043dac922c1c3a7e42
JewelDrop <eduardojsczerniak@yahoo.com>
2024-11-25 23:49:56 -0300

    Commit 9: Implementação de getFrequentRenterPoints em Customer

---
+++ Customer.java
@@ -0,0 +1,51 @@
+import java.util.Enumeration;
+import java.util.Vector;
+
+public class Customer {
+    private String _name;
+    private Vector _rentals = new Vector();
+
+    public Customer(String name) {
+        _name = name;
+    }
+
+    public void addRental(Rental rental) {
+        _rentals.addElement(rental);
+    }
+
+    public String getName() {
+        return _name;
+    }
+
+    public String statement() {
+        return new TextStatement().value(this);
+    }
+
+    public String htmlStatement() {
+        return new HtmlStatement().value(this);
+    }
+
+    public Enumeration getRentals() {
+        return _rentals.elements();
+    }
+
+    public double getTotalCharge() {
+        double result = 0;
+        Enumeration rentals = _rentals.elements();
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += each.getCharge();
+        }
+        return result;
+    }
+
+    public int getTotalFrequentRenterPoints() {
+        int result = 0;
+        Enumeration rentals = _rentals.elements();
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += each.getFrequentRenterPoints();
+        }
+        return result;
+    }
+}
